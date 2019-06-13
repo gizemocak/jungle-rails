6931babec2f8cc809183b656d123b50156bb5812
gizemocak <gizem_ocak@outlook.com>
2019-06-13 00:51:38 +0000

    register form and database

--- config/routes.rb
+++ config/routes.rb
@@ -11,11 +11,12 @@ Rails.application.routes.draw do
   end
 
   resources :orders, only: [:create, :show]
+  resources :register
 
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
-    resources :categories, except: [:edit, :update, :show]
+    resources :categories, except: [:edit, :update, :show] 
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
