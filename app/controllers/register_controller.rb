97f33be612205bc5f0bd259d06d30c0c5d029a8b
gizemocak <gizem_ocak@outlook.com>
2019-06-13 03:46:18 +0000

    user can register

--- app/controllers/register_controller.rb
+++ app/controllers/register_controller.rb
@@ -1,4 +1,29 @@
 class RegisterController < ApplicationController
   def index
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to [:admin, :products]
+    else
+      render :index
+    end
+  end
+
+  private 
+  
+  def user_params
+    params.require(:register).permit(
+      :email,
+      :password,
+      :first_name,
+      :last_name
+    )
+  end
 end
