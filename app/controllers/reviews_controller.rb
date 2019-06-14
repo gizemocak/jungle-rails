32def3d1bd968d9500d5bf4688d4d6b200bd77b1
gizemocak <gizem_ocak@outlook.com>
2019-06-14 19:44:07 +0000

    save reviews to the database and display

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,21 @@
 class ReviewsController < ApplicationController
   def create
-    raise "Yay, I'm here!"
+    @review = Product.find(params[:product_id]).reviews.new(review_params)
+    @review.user = current_user
+
+    if @review.save
+      # redirect_to "/products/#{params[:product_id]}"
+      redirect_to product_path params[:product_id]
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(
+      :description
+    )
   end
 end
