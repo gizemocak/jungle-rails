6bad1bac22ffe91f44c24697959d04d4c276f897
gizemocak <gizem_ocak@outlook.com>
2019-06-18 14:39:38 -0400

    more robust tests- fixing broken images

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -9,9 +9,10 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
       @category.products.create!(
         name:  Faker::Hipster.sentence(3),
         description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
         quantity: 10,
         price: 64.99
-      )
+      )      
     end
   end
 
@@ -19,9 +20,9 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
     # ACT
     visit root_path
 
-    # DEBUG / VERIFY
-    save_screenshot
-    expect(page).to have_css 'article.product'
+    # commented out b/c it's for debugging only
+    save_and_open_screenshot
+    expect(page).to have_css 'article.product', count:10
   end
 
 end
